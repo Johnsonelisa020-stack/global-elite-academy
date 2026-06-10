7fe63b7ea03c1c07f52e5b34cc3334d3fd652257
Johnsonelisa020-stack <Johnsonelisa020@gmail.com>
2026-06-09 23:07:18 -0700

    Create constants.js

---
+++ src/constants.js
@@ -0,0 +1,4 @@
+export const T = { /* Hapa weka ile dictionary yako yote ya T */ };
+export const PAST_PAPERS = [ /* Hapa weka array ya PAST_PAPERS */ ];
+export const NOTES = [ /* Hapa weka array ya NOTES */ ];
+export const TEACHERS = [ /* Hapa weka array ya TEACHERS */ ];
